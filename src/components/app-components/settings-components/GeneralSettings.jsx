1e38c251cefd074319d2c99509c4d7ac71983bae
Hasala2002 <hasala.h@hotmail.com>
2023-12-29 01:42:27 -0600

    Scroll Issue in settings fixed

--- src/components/app-components/settings-components/GeneralSettings.jsx
+++ src/components/app-components/settings-components/GeneralSettings.jsx
@@ -3,6 +3,7 @@ import { useSettings } from '../../utilities/Settings.Context'
 import * as styles from "./styles/SettingsComponent.module.scss"
 import ChatMessage from '../chat-components/ChatMessage'
 import ChatOut from '../chat-components/ChatOut'
+import Divider from "../../utilities/Divider"
 
 const GeneralSettings = () => {
 
@@ -19,6 +20,8 @@ const GeneralSettings = () => {
         setToastState(!toastState)
       }} id="toastSet" /><label htmlFor="toastSet">Toggle</label>
 
+      <Divider />
+
       <h3>Sounds</h3>
       <span>You can switch off sound notifications here.</span>
       <input type="checkbox" checked={soundState} onChange={() => {
@@ -26,6 +29,7 @@ const GeneralSettings = () => {
         setSoundState(!soundState)
       }} id="sounds" /><label htmlFor="sounds">Toggle</label>
 
+      <Divider />
 
       <h3>Message Text Size</h3>
       <span>Adjust font sizing to your preference here. <b>{"("}Default set at 100%{")"}</b></span>
@@ -42,12 +46,18 @@ const GeneralSettings = () => {
           localStorage.setItem("text_size", parseInt(e.target.value))
         }} />
       </div>
+
+      <Divider />
+
       <h3>Time Format</h3>
       <span>You can enable 12-hour format timestamps here.</span>
       <input type="checkbox" checked={twelveHrClock} onChange={() => {
         localStorage.setItem("twelve_hour", twelveHrClock ? "off" : "on")
         setTwelveHrClock(!twelveHrClock)
       }} id="twelvehour" /><label htmlFor="twelvehour">Toggle</label>
+
+      {/* <Divider /> */}
+
       <div className={styles.fontPreview}>
         <span className={styles.fontPreviewTitle}>chat bubble preview</span>
         <div>
